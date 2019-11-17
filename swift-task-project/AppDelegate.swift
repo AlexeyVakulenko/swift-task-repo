1635b15c4633df6285f51fe7df4122ee2395c4ce
Aleksey Vakulenko <vakulenko.a.vla@sberbank.ru>
2019-11-17 18:22:58 +0300

    add table view

--- swift-task-project/AppDelegate.swift
+++ swift-task-project/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let tasksViewController = TasksViewController()
+        let navigationController = UINavigationController(rootViewController: tasksViewController)        
+        navigationController.viewControllers = [tasksViewController]
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 
